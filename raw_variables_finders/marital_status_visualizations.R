7d319e9feb50fed6900ded623e8c4e4e8eed399f
Haylie <turnerh1@spu.edu>
2022-04-15 13:51:20 -0700

    created initial visualization and tidied marriage data

--- raw_variables_finders/marital_status_visualizations.R
+++ raw_variables_finders/marital_status_visualizations.R
@@ -5,41 +5,58 @@ options(tigris_use_cache = TRUE)
 # load job_access_gap, but leave out the geometries
 job_access_gap <- read_csv("job_access_gap.csv", 
                            col_types = cols(geometry = col_skip()))
+#convert job_access_gap data to have census tracts
 
-get_variables = c("B12001_001","B12001_002","B12001_003","B12001_004","B12001_005","B12001_006","B12001_007","B12001_008","B12001_009","B12001_010","B12001_011","B12001_012","B12001_013","B12001_014","B12001_015","B12001_016","B12001_017","B12001_018","B12001_019")
+job_access_gap <- job_access_gap %>%
+  rename("BG_GEOID"="GEOID")
+job_access_gap$BG_GEOID<-as.character(job_access_gap$BG_GEOID)
 
-marital_census <- get_acs(geography = "block group", variables = get_variables, state = "WA", geometry = T)
+T_GEOID<-substr(job_access_gap$BG_GEOID,1,11)
 
-marital_census <- marital_census%>%
-  mutate(GEOID = as.numeric(GEOID)) 
+job_access_gap <- cbind(job_access_gap,T_GEOID)
 
-# import variables_2019 
-variables_2019 <- read_csv("variables_2019.csv")
+# import marital_status_data by running file 'marital_status_all.R"
 
-# rename variable code to be able to join
-variables_2019<-rename(variables_2019,"variable"="name")
+marital_status_data <- marital_status_data %>%
+  rename("T_GEOID" = "GEOID")
 
-# join to get variable names for census code
-marital_variables<-left_join(marital_census,variables_2019,by="variable") %>%
-  select(-"...1")
+# join to marital and job access data
+
+marital_job_access<-left_join(marital_status_data,job_access_gap, by = "T_GEOID") 
+marital_job_access$T_GEOID<-as.numeric(marital_job_access$T_GEOID)
+
+# total now married not including separated
+married_not_sep <- marital_job_access %>%
+  filter(grepl("S1201_C02_001",variable))
+
+# choose colors: http://colorbrewer2.org/ 
+#colors and breaks
+mybreaks<-c(0,25,50,75,100)
+my_colors <- c("white","#fcae91","#fb6a4a","#de2d26","#a50f15")
 
 
-marital_status<-left_join(marital_variables,job_access_gap,by="GEOID")
-
-#need to do filter into specific marraige types before able to plot
-never_married_male <- marital_status %>%
-  filter(grepl("Estimate!!Total:!!Male:!!Never married",label))
-
-
-# plot of never married males in sea
-ggplot(never_married_male) +
+# plot of now married
+ggplot(married_not_sep) +
   geom_sf(color="#C0C0C0",aes(fill = estimate), size=0.00001)+
   scale_fill_gradientn(colors=my_colors,
                        na.value = "transparent",
                        breaks=mybreaks,
                        labels = mybreaks)+
-  labs(title="Spatial Mismatch By Census Block Group",
-       subtitle = "Seattle, WA and Surrounding Areas")+
+  labs(title="Married in Seattle by Tract Group",
+       subtitle = "15+ in age")+
   theme(plot.title = element_text(hjust = 0.5, size=15)) +
   theme(plot.subtitle = element_text(hjust = 0.5, size=10))+
-  labs(fill = "Normalized\nSpatial\nMismatch") 
+  labs(fill = "estimate") 
+
+
+# same thing but for widowed, divorced, separated, never married:
+widowed <- marital_job_access %>%
+  filter(grepl("S1201_C03_001",variable))
+divorced <- marital_job_access %>%
+  filter(grepl("S1201_C04_001",variable))
+separated <- marital_job_access %>%
+  filter(grepl("S1201_C05_001",variable))
+nevermarried <- marital_job_access %>%
+  filter(grepl("S1201_C06_001",variable))
+population <- marital_job_access %>%
+  filter(grepl("S1201_C01_001",variable))

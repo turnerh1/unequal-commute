22c687bda21330509a7ebe25a0cafc47960ea6df
Haylie <turnerh1@spu.edu>
2022-04-20 15:53:25 -0700

    created file for all data

---
+++ raw_variables_finders/all_census_data.R
@@ -0,0 +1,6 @@
+Joining all vars
+# units of measurement 
+# marital status - count of people
+# language - number of people who speak per group
+# num of household earners - number of earners per family
+# 

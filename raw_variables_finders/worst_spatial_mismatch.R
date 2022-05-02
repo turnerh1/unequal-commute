d949ec8678c9f221fbacaa7cb46dae6fdff530cc
Haylie <turnerh1@spu.edu>
2022-05-02 15:35:45 -0700

    skeleton code for getting top and bottom 10 percent

--- raw_variables_finders/worst_spatial_mismatch.R
+++ raw_variables_finders/worst_spatial_mismatch.R
@@ -1,16 +1,29 @@
+library(tidycensus)
 library(tidyverse)
+
+job_access_gap <- read_csv("job_access_gap.csv")
 
 sea_gap <- job_access_gap %>%
   filter(MSA == "Seattle")
 #join table
 
+#calculate how many are in 10% of data
+quantity<-round(.1*dim(sea_gap)[1])
 
-lower<-summary(sea_gap$spatialmismatch)[2]
-higher<-summary(sea_gap$spatialmismatch)[5]
+high<-sea_gap %>%
+  arrange(desc(spatialmismatch))%>%
+  head(quantity)
 
-low_sea_gap <- sea_gap %>%
-  filter(spatialmismatch <= lower)
+low<-sea_gap %>%
+  arrange((spatialmismatch))%>%
+  head(quantity)
 
-high_sea_gap <- sea_gap %>%
-  filter(spatialmismatch >= higher)
-#table ideas
+#import big data CSV
+library(readr)
+acs_dataset <- read_csv("acs_dataset.csv")
+View(acs_dataset)
+
+#select the columns you want
+
+
+

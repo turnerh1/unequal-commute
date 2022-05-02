86b8f8ff5031d2bf33cc6fdc8f92ab7c188e741d
Haylie <turnerh1@spu.edu>
2022-05-02 15:46:20 -0700

    creating marital status vis

--- raw_variables_finders/marital_status_visualizations.R
+++ raw_variables_finders/marital_status_visualizations.R
@@ -29,3 +29,30 @@ ggplot() +
   theme(plot.title = element_text(hjust = 0.5, size=15)) +
   theme(plot.subtitle = element_text(hjust = 0.5, size=10))+
   labs(fill = "Percentage") 
+###########################
+library(tidycensus)
+library(tidyverse)
+
+job_access_gap <- read_csv("job_access_gap.csv")
+
+sea_gap <- job_access_gap %>%
+  filter(MSA == "Seattle")
+#join table
+
+#calculate how many are in 10% of data
+quantity<-round(.1*dim(sea_gap)[1])
+
+high<-sea_gap %>%
+  arrange(desc(spatialmismatch))%>%
+  head(quantity)
+
+low<-sea_gap %>%
+  arrange((spatialmismatch))%>%
+  head(quantity)
+
+#import big data CSV
+library(readr)
+acs_dataset <- read_csv("acs_dataset.csv")
+View(acs_dataset)
+
+#join with marital status data

b497fa9c5e8b5bb39dc36672d951fa37667ff71b
Haylie <turnerh1@spu.edu>
2022-05-02 17:25:33 -0700

    worst spat.mismatch by marraige status

--- raw_variables_finders/marital_status_visualizations.R
+++ raw_variables_finders/marital_status_visualizations.R
@@ -37,7 +37,6 @@ job_access_gap <- read_csv("job_access_gap.csv")
 
 sea_gap <- job_access_gap %>%
   filter(MSA == "Seattle")
-#join table
 
 #calculate how many are in 10% of data
 quantity<-round(.1*dim(sea_gap)[1])
@@ -53,6 +52,38 @@ low<-sea_gap %>%
 #import big data CSV
 library(readr)
 acs_dataset <- read_csv("acs_dataset.csv")
-View(acs_dataset)
+marital_census <- acs_dataset %>%
+  select(GEOID,NAME,contains("B1200"))
 
 #join with marital status data
+marital_status_high<-left_join(high,marital_census,by="GEOID")
+
+#clean and create new columns combining sex
+marital_status_high$nevermarried = marital_status_high$estimate_B12001_003 + marital_status_high$estimate_B12001_012
+marital_status_high$marriednow=marital_status_high$estimate_B12001_004+marital_status_high$estimate_B12001_013
+marital_status_high$widowed=marital_status_high$estimate_B12001_009+marital_status_high$estimate_B12001_018
+marital_status_high$divorced=marital_status_high$estimate_B12001_010+marital_status_high$estimate_B12001_019
+total_single=sum(marital_status_high$nevermarried)
+total_married=sum(marital_status_high$marriednow)
+total_widowed=sum(marital_status_high$widowed)
+total_divorced=sum(marital_status_high$divorced)
+total_pop=sum(marital_status_high$nevermarried,total_married,total_widowed,total_divorced)
+
+totals<-data.frame(marital_status = c("Single","Married","Widowed","Divorced","Total"),
+               counts = c(total_single, total_married, total_widowed, total_divorced, total_pop))
+totals$prop<- df$counts / df$counts[5]
+
+#fun colors:
+# https://r-graph-gallery.com/38-rcolorbrewers-palettes.html
+
+totals %>%
+  filter(marital_status != "Total") %>%
+  ggplot()+
+  geom_col(aes(x=marital_status,y=prop,fill=marital_status)) +
+  xlab("Marital Status") + 
+  ylab("Proportion of Population (ages 15+)")+
+  labs(title= "Marital Status Proportions for Highest 10%\n(Worst) Spatial Mismatch in Seattle") +
+  theme(plot.title = element_text(hjust = 0.5),
+        panel.background = element_rect(fill = '#f7f7f7'),
+        legend.position = "none")+
+  scale_fill_brewer(palette = "Set3")

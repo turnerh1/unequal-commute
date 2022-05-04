c8afa0023589bc4e83434c8a1648dc1cec481e7d
katiefaith <katie.of.the.mountains@gmail.com>
2022-05-04 15:34:44 -0700

    Join our two primary datasets, filter to high/low spatial mismatch for visualization

---
+++ raw_variables_finders/visualization_setup.R
@@ -0,0 +1,25 @@
+library(tidycensus)
+library(tidyverse)
+library(readr)
+
+job_access_gap <- read_csv("./data/job_access_gap.csv")
+
+sea_gap <- job_access_gap %>%
+  filter(MSA == "Seattle")
+
+#calculate how many are in 10% of data
+ten_pct_quantity<-round(.1*dim(sea_gap)[1])
+
+high<-sea_gap %>%
+  arrange(desc(spatialmismatch))%>%
+  head(ten_pct_quantity)
+
+low<-sea_gap %>%
+  arrange((spatialmismatch))%>%
+  head(ten_pct_quantity)
+
+#import big data CSV
+acs_dataset <- read_csv("./data/acs_dataset.csv")
+
+high_tenpct <- left_join(high,acs_dataset,by="GEOID")
+low_tenpct <- left_join(low,acs_dataset,by="GEOID")

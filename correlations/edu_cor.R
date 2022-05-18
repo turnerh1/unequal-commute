128f722dd37e299859edf39b8c68f2cbeacdbb31
Jobi Lo <102559432+jobilo@users.noreply.github.com>
2022-05-17 17:05:06 -0700

    make new categories for education levels

--- correlations/edu_cor.R
+++ correlations/edu_cor.R
@@ -1,5 +1,9 @@
 library(dplyr)
 library(openintro)
+library(corrplot)
+
+acs_dataset <- read_csv("data/acs_dataset.csv")
+job_access_gap <- read_csv("data/job_access_gap.csv")
 
 edu_census <- acs_dataset %>%
   select(GEOID,NAME,contains("B15003"))
@@ -36,8 +40,17 @@ edu_census$post_grad = edu_census$estimate_B15003_023 +
   edu_census$estimate_B15003_024 + 
   edu_census$estimate_B15003_025
 
+#group above and below Bachelor's level
+edu_census$below_bach <- edu_census$estimate_B15003_002 + edu_census$estimate_B15003_016 + edu_census$estimate_B15003_017 + 
+                        edu_census$estimate_B15003_018 + edu_census$estimate_B15003_019 + edu_census$estimate_B15003_020 +
+                        edu_census$estimate_B15003_021
+edu_census$above_bach <- edu_census$estimate_B15003_022 + edu_census$estimate_B15003_023 + edu_census$estimate_B15003_024 +
+                        edu_census$estimate_B15003_025
+
 edu_spatial <- left_join(job_access_gap, edu_census,by = "GEOID") %>%
-  select(-geometry,-NAME)
+  select(-geometry,-NAME) %>%
+  filter(MSA == "Seattle")
+
 
 #create proportions
 edu_spatial$prop_noedu <- edu_spatial$estimate_B15003_002/edu_spatial$estimate_B15003_001
@@ -56,8 +69,14 @@ edu_spatial$prop_somecollege <- edu_spatial$somecollege/edu_spatial$estimate_B15
 edu_spatial$prop_higher_ed <- edu_spatial$higher_ed/edu_spatial$estimate_B15003_001
 edu_spatial$prop_post_grad <- edu_spatial$post_grad/edu_spatial$estimate_B15003_001
 
+edu_spatial$prop_below_bach <- edu_spatial$below_bach/edu_spatial$estimate_B15003_001
+edu_spatial$prop_above_bach <- edu_spatial$above_bach/edu_spatial$estimate_B15003_001
+
 edu_spatial_cor <- edu_spatial%>%
   select(spatialmismatch,prop_somecollege,prop_higher_ed,prop_post_grad)
+
+edu_spatial_2var <- edu_spatial%>%
+  select(spatialmismatch,prop_below_bach,prop_above_bach)
 
 #plotting all categories vs only upper
 m.all <- lm(spatialmismatch~prop_noedu+prop_nodiploma+prop_hsdiploma+prop_GED+prop_lesssomecollege+prop_moresomecollege+prop_AA+prop_BS+prop_MD+prop_pro+prop_PhD, data = edu_spatial)
@@ -66,6 +85,8 @@ m.higher_edu <- lm(spatialmismatch~prop_BS+prop_MD+prop_pro+prop_PhD, data = edu
 summary(m.all)
 summary(m.higher_edu)
 anova(m.all, m.higher_edu)
+
+
 
 # basic correlations
 edu_correlation = cor(edu_spatial_cor[,(1:4)], use = "complete.obs")
@@ -81,8 +102,8 @@ pairs(edu_spatial_cor[,(1:4)])
 
 # modeling categorized education levels (Best so far)
 m.cat <- lm(spatialmismatch~somecollege + higher_ed + post_grad, data = edu_spatial)
-
-summary(m.cat)
+m.propcat <- lm(spatialmismatch~prop_somecollege + prop_higher_ed + prop_post_grad, data = edu_spatial)
+summary(m.propcat)
 
 plot(spatialmismatch~prop_higher_ed, data = edu_spatial)
 m.some_higher <- lm(spatialmismatch~prop_somecollege + prop_higher_ed + prop_somecollege*prop_higher_ed, data = edu_spatial)
